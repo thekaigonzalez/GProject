c4109b5e29c1ba57f8f47219a975baaf4cd4923e
Kai Gonzalez <gkai70264@gmail.com>
2021-10-05 18:10:23 -0400

    Add SubMods and new GMacro update

--- lib/gmacro.d
+++ lib/gmacro.d
@@ -1,17 +1,16 @@
 module lib.gmacro;
-
 /**
 
 Copyright (C) 2021 Kai D. Gonzalez
 
  This file is part of GMacro.
 
- GMacro is free software: you can redistribute it and/or 
- modify it under the terms of the GNU General Public License as published by the Free Software Foundation, 
+ GMacro is free software: you can redistribute it and/or
+ modify it under the terms of the GNU General Public License as published by the Free Software Foundation,
  either version 3 of the License, or (at your option) any later version.
 
- GMacro is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
- without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ GMacro is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  See the GNU General Public License for more details.
 
  You should have received a copy of the GNU General Public License along with GMacro. If not, see http://www.gnu.org/licenses/.
@@ -29,25 +28,31 @@ The main Parser Class
 */
 class GM_Parser {
     /// All of the stuff to be returned.
-	string[string] all_stuff;
-	string pkey; /// ....
+        string[string] all_stuff;
+        string pkey; /// ....
 public:
-	this(string p) {
+        this(string p) {
         pkey=p;
-	}
+        }
     string[string] defineonce() {
         string[] each_statement = pkey.split(";");
         foreach(string stat; each_statement) {
-            if (stat.length > 1) {
+            if (stat.startsWith("#")) {
+                            continue;
+            }
+            if (stat.length > 1 && !startsWith(stat, ";")) {
                 string fname = stat[0 .. stat.indexOf("(")];
                 string fargs = stat[stat.indexOf("(")+1 .. stat.indexOf(")")];
                 fargs = fargs.strip();
                 fname = fname.strip();
-                all_stuff[fname] = fargs; 
+                if (fname in all_stuff) {
+                    writeln("warning:gmacro.d:47: same property defined twice");
+                }
+                all_stuff[fname] = fargs;
             }
-		}
-        return all_stuff;
-	}
+        }
+            return all_stuff;
+    }
 }
 
 class GM_ArgPasser {
@@ -55,14 +60,14 @@ class GM_ArgPasser {
 public:
     this(string[string] psu) {
         pass = psu;
-	}
+        }
     string[] getonce(string func) {
         if (func in pass) {
             return pass[func].split(",");
         } else {
             return ["NULL"];
         }
-	}
+        }
 }
 
 class SGMacroFile {
@@ -70,12 +75,12 @@ class SGMacroFile {
 public:
     this(string fnile) {
         fname=fnile;
-	}
+        }
     string[string] parseOnce() {
         if (!fname.exists) {
             writeln("GMacro: Couldn't open input file.");
             return ["none": "ERROR"];
-		}
+                }
         File f = File(fname, "r");
         string cpp;
 
@@ -83,7 +88,7 @@ public:
             string l = f.readln();
             if (l.length > 1)
                 cpp = cpp~l;
-		}
+                }
         return new GM_Parser(cpp).defineonce();
-	}
+        }
 }
--- source/gproject.d
+++ source/gproject.d
@@ -7,23 +7,8 @@ import std.array;
 
 import lib.gmacro;
 
-int main(string[] args) {
-    if (args.length == 2) {
-        if (args[1] == "-help" || args[1] == "help") {
-            writeln("usage: "~args[0]~" [-help] [-version] [run]");
-        }
-    } 
-    if (args.length == 3) {
-        if (args[1] == "run") {
-            string projname = args[2];
-            writeln("gproject-load: Loading "~projname~"...");
-            writeln("gproject-load: Checking for "~projname~".gm ...");
-            if (projname~".gm".exists) {
-                
-                writeln("gproject-load: Building "~projname~"...");
-                
-                /// Use SGMacroFile API to load a file.
-                SGMacroFile sgmf = new SGMacroFile(projname~".gm");
+int loader(string[] args) {
+                SGMacroFile sgmf = new SGMacroFile("GSub");
                 auto projinfo = sgmf.parseOnce();
                 if ("none" in projinfo) {
                     writeln("Error: Problem loading file.\n err: expected COMPILER, got NONE!");
@@ -40,15 +25,26 @@ int main(string[] args) {
                 string[] flagsargs = gmap.getonce("FLAGS");
                 string flags = flagsargs[0];
                 if (flagsargs[0] == "NULL") {
-                    writeln("gproject-load: Couldn't find the FLAGS macro, is this a project macro file?");
-                    return -1;
+                    if (args[1] == "-stricttags") {
+                        writeln("gproject-load: Couldn't find the FLAGS macro, is this a project macro file?");
+                        return -1;
+                        
+                    } else {
+                        writeln("gproject-load: pretending there are no flags required.");
+                        flags = "";
+                    }
+                    
                 }
                 string[] srcs = gmap.getonce("SRC");
                 if (srcs[0] == "NULL") {
-                    writeln("gproject-load: Couldn't find the SRC macro, is this a project macro file?");
-                    return -1;
+                        writeln("gproject-load: pretending there are no sources.");
+                        srcs = [];
                 }
-                
+                string[] subs = gmap.getonce("SUBMODS");
+                if (subs[0] == "NULL") {
+                    writeln("gproject-load: ignoring submods.");
+                    subs = [];
+                }
 
                 writeln("gproject-load: Got project information");
                 
@@ -56,23 +52,119 @@ int main(string[] args) {
                 writeln("Flags: "~flags);
                 write("Sources: ");
                 writeln(srcs);
+                write("Sub Modules: ");
+                writeln(subs);
 
                 writeln("gproject: Is this correct?");
                 write("y/n ? ");
                 string inp = readln();
                 if (inp == "y\n") {
                     foreach (string source; srcs) {
-                        writeln("building source "~source);
-                        system((compilerdata~" "~flags~" "~source).toStringz());
+                        if (source == "NULL") continue;
+                        else {
+                            writeln("building source "~source);
+                            system((compilerdata~" "~flags~" "~source).toStringz());
+                        }
+                    }
+                    writeln("gproject: Running submodules...");
+                    foreach (string sub; subs) {
+                        writeln("gproject-submod: entering directory - "~sub);
+                        try {
+                            chdir(sub);
+                            loader(args);
+                        } catch (Exception e) {
+
+                        }
                     }
                 } else {
                     writeln("Ok. Project cancelled!");
                 }
+                return 0;
+}
+
+int main(string[] args) {
+    if (args.length == 2) {
+        if (args[1] == "-help" || args[1] == "help") {
+            writeln("usage: "~args[0]~" [-help] [-version] [run]");
+        }
+    } 
+    if (args.length == 2) {
+        if (args[1] == "run") {
+            writeln("gproject-load: Loading ...");
+            writeln("gproject-load: Checking for GProject ...");
+            if ("GProject".exists) {
+                
+                writeln("gproject-load: Building ...");
+                
+                /// Use SGMacroFile API to load a file.
+                SGMacroFile sgmf = new SGMacroFile("GProject");
+                auto projinfo = sgmf.parseOnce();
+
+                
+                if ("none" in projinfo) {
+                    writeln("Error: Problem loading file.\n err: expected COMPILER, got NONE!");
+                    return -1;
+                }
+                /// pass project info into Argument handler
+                GM_ArgPasser gmap = new GM_ArgPasser(projinfo);
+                string[] compilermacro = gmap.getonce("COMPILER");
+                if (compilermacro[0] == "NULL") {
+                    writeln("gproject-load: Couldn't find the COMPILER macro, is this a project macro file?");
+                    return -1;
+                }
+                string compilerdata = compilermacro[0]; // first argument
+                string[] flagsargs = gmap.getonce("FLAGS");
+                string flags = flagsargs[0];
+                if (flagsargs[0] == "NULL") {
+                    if (args[2] == "-stricttags") {
+                        writeln("gproject-load: Couldn't find the FLAGS macro, is this a project macro file?");
+                        return -1;
+                        
+                    } else {
+                        writeln("gproject-load: pretending there are no flags required.");
+                    }
+                    
+                }
+                string[] srcs = gmap.getonce("SRC");
+                if (srcs[0] == "NULL") {
+                        writeln("gproject-load: pretending there are no sources.");
+                        srcs = [];
+                    
+                }
+                string[] subs = gmap.getonce("SUBMODS");
+                if (subs[0] == "NULL") {
+                    writeln("gproject-load: ignoring submods.");
+                    subs = [];
+                }
+
+                writeln("gproject-load: Got project information");
+                
+                    foreach (string source; srcs) {
+                        if (source == "NULL") {
+                            continue;
+                        }
+                        writeln("building source "~source);
+                        system((compilerdata~" "~flags~" "~source).toStringz());
+                    }
+                    writeln("gproject: Running submodules...");
+                    foreach (string sub; subs) {
+                        writeln("gproject-submod: entering directory - "~sub);
+                        try {
+                            if (sub == "NULL") {
+                                continue;
+                            }
+                            chdir(sub);
+                            loader(args);
+                        } catch (Exception e) {
+                            writeln("gproject-load: Submodule not found.");
+                        }
+                    }
+                }
+
             } else {
-                writeln("gproject-load: couldn't find a "~projname~".gm file in the current directory.");
+                writeln("GProject Failed.");
                 return -1;
             }
         }
-    }
     return 0;
 }
